8effc2a8280eadbdcfbc743b5b9735fb89d19959
Rodolffo Teles <rodolffoteles@hotmail.com>
2020-05-21 13:54:05 -0300

    Create route for updating and deleting products

--- routes/index.js
+++ routes/index.js
@@ -4,33 +4,53 @@ const router = express.Router();
 
 // Retrive a list with all products
 router.get('/', async (req, res) => {
-  let products = await db.Product.findAll();
+  let products = await db.Product.findAll({ include: db.Category });
   
-  return res.status(200).json(products, { include: db.Category });
+  return res.status(200).json(products);
 });
 
 // Retrive a specific product
 router.get('/:id', async (req, res) => {
   let product = await db.Product.findByPk(req.params.id, { include: db.Category });
   
-  return res.status(200).json(product);
+  if (!product){
+    return res.status(404).end();
+  } else {
+    return res.status(200).json(product);
+  }
 });
 
 // Create a new product
 router.post('/', async (req, res) => {
-  await db.Product.create(req.body, { fields: ['description', 'categoryId'] });
+  let product = await db.Product.create(req.body, { fields: ['description', 'categoryId'] });
 
-  return res.status(201).end();
+  return res.location(`/products/${product.id}`).status(201).end();
 });
 
 // Update a product
-router.put('/', async (req, res) => {
- // TO DO: Implement update route
+router.put('/:id', async (req, res) => {
+ let product = await db.Product.findByPk(req.params.id);
+
+ if (!product) {
+   return res.status(404).end();
+ }
+
+ product.update(req.body, { fields: ['description', 'categoryId'] })
+
+ return res.status(204).end();
 });
 
 // Delete one product
-router.delete('/', async (req, res) => {
-  // TO DO: Implement delete route
+router.delete('/:id', async (req, res) => {
+  let product = await db.Product.findByPk(req.params.id);
+
+  if (!product) {
+    return res.status(404).end();
+  }
+
+  product.destroy();
+
+  return res.status(204).end();
 });
 
 module.exports = router;
