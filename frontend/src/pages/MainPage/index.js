9f47e46f94acff0d79d5ad08f53d9a79832f75d9
Rodolffo Teles <rodolffoteles@hotmail.com>
2020-05-24 22:33:25 -0300

    Initial frontend commit

---
+++ frontend/src/pages/MainPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MainPage.scss';
+
+import SidePanel from '../../components/SidePanel';
+
+const MainPage = () => {
+  return (
+    <main>
+      <SidePanel/>
+    </main>
+  )
+};
+
+export default MainPage;
